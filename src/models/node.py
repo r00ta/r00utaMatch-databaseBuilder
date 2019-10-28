f4e83ba696f9f3c55bf2aa8dd9051e227f3bc718
Jacopo <jacopo.r00ta@gmail.com>
2019-10-28 21:20:06 +0100

    add intersection detection

--- src/models/node.py
+++ src/models/node.py
@@ -3,7 +3,12 @@ class Node():
     def __init__(self, location, positive_id):
         self.location = location
         self.positive_id = positive_id
+        self.is_intersection = False
 
-    def get_location_dict(self):
-        """ Get the location of a node (lat/lon) as dict. """
-        return {'lat' : self.location.lat, 'lon' : self.location.lon}
+    def serialize(self):
+        """ Get serialized object. """
+        return {'lat' : self.location.lat, 'lon' : self.location.lon, 'is_intersection' : self.is_intersection}
+
+    def set_is_intersection(self, value):
+        """ Set is_intersection property """
+        self.is_intersection = value
